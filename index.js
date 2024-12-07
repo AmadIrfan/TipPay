0d4147ea14ea05b28c5157517f6d804acda3a9d7
amadirfan <amadirfan443@gmail.com>
2024-12-07 21:13:54 +0500

    final changes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const twilio = require('twilio')
 require('dotenv').config();
 
 // Database Connection
